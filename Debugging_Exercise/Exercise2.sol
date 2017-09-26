ca58b9e7533da63378fb5fe1b9267eb9be99b0f7
Kishan Sagathiya <10185999+kishansagathiya@users.noreply.github.com>
2017-09-26 14:17:50 +0000

    Update Exercise2.sol

--- Debugging_Exercise/Exercise2.sol
+++ Debugging_Exercise/Exercise2.sol
@@ -1,22 +1,21 @@
 pragma solidity ^0.4.5;
 
 contract WarehouseI {
-    function setDeliveryAddress(string where) public ;
-    function ship(uint id, address customer) public returns (bool handled);
+    function setDeliveryAddress(string where);
+    function ship(uint id, address customer) returns (bool handled);
 }
 
 contract Store {
     address wallet;
     WarehouseI warehouse;
 
-    function Store(address _wallet, address _warehouse) public{
+    function Store(address _wallet, address _warehouse) {
         wallet = _wallet;
         warehouse = WarehouseI(_warehouse);
     }
 
-    function purchase(uint id) payable public returns (bool success) {
-        wallet.transfer(msg.value);
-        if(!warehouse.ship(id, msg.sender)) revert();
-        success = true;
+    function purchase(uint id) returns (bool success) {
+        wallet.send(msg.value);
+        return warehouse.ship(id, msg.sender);
     }
 }
